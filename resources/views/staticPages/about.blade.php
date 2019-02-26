622684b2c390b5cb662007752871a4742fb9bc3a
vagrant <vagrant@homestead>
2019-02-26 14:23:25 +0000

    up

---
+++ resources/views/staticPages/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+<h1>关于页</h1>
+@stop
---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//静态页面路由
+Route::get('/', 'StaticPagesController@home')->name('home');
+Route::get('/help', 'StaticPagesController@help')->name('help');
+Route::get('/about', 'StaticPagesController@about')->name('about');
+
+//注册
+Route::get('/signup', 'UsersController@create')->name('signup');
